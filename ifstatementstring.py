64438e2c87499e1c1b0d787f8704a691afa425a1
Philipp Bischof <philipp.bischof@phb-it.de>
2019-12-12 19:38:45 +0100

    Max ist jetzt Julia

--- ifstatementstring.py
+++ ifstatementstring.py
@@ -2,7 +2,7 @@ name = input('Wie ist dein Name?')
 
 if name == "Philipp":
     print('Hallo Administrator')
-elif name == "Max":
+elif name == "Julia":
     print('Hallo Max')
 else:
     print('Du bist kein Administrator')
