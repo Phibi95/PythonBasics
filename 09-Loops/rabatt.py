54d3e392683a8349f1ea8595bc25ee05235a84a0
Philipp Bischof <philipp.bischof@phb-it.de>
2019-12-16 18:02:34 +0100

    Init lession 09

---
+++ 09-Loops/rabatt.py
@@ -0,0 +1,13 @@
+price = 490
+
+#In Prozent
+discount_input = input("Wie hoch ist der Rabatt?: (in Prozent) ")
+
+discount = price / 100 * float(discount_input)
+
+price = price - discount
+
+
+text = "Dein finaler Preis lautet: "
+
+print(text + str(price))
