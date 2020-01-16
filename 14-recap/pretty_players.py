0ae1563f794d67d6004530a2ae9dbc378408eb13
Philipp Bischof <philipp.bischof@phb-it.de>
2020-01-16 19:34:00 +0100

    Session 14

--- 14-recap/pretty_players.py
+++ 14-recap/pretty_players.py
@@ -1,3 +1,5 @@
+import prettytable
+
 class Player():
     def __init__(self,first_name, last_name, height_cm, weight_kg):
         self.first_name = first_name
@@ -30,5 +32,19 @@ player_1 = BasketballPlayer(first_name="Lebron",last_name="James",height_cm=203,
 
 player_2 = BasketballPlayer(first_name="Kevin", last_name="Durant", height_cm=210, weight_kg=108, points=27.2,
                            rebounds=7.1, assists=4)
+player_3 = BasketballPlayer(first_name="Luka", last_name="Doncic", height_cm=201, weight_kg=103, points=20,
+                           rebounds=6.7, assists=5)
+player_4 = BasketballPlayer(first_name="James", last_name="Harden", height_cm=196, weight_kg=99, points=23.6,
+                           rebounds=5.1, assists=6.2)
 
-basketball_players = [player_1,player_2]
+basketball_players = [player_1,player_2,player_3,player_4]
+
+table = prettytable.PrettyTable()
+
+table.field_names = ["first_name", "last_name", "height_cm", "weight_lbs", "points", "rebounds", "assists"]
+
+for player in basketball_players:
+    table.add_row([player.first_name, player.last_name, player.height_cm, player.get_weight_in_lbs(), player.points,
+                   player.rebounds, player.assists])
+
+print(table)
