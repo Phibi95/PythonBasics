6e4ea687970762a7e72074095b8ad1c54d3fc59b
Philipp Bischof <philipp.bischof@phb-it.de>
2020-02-17 18:37:20 +0100

    Session 22

---
+++ 22-automatic-testing/tests/test_main.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+from main import app, db
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    os.environ["DATABASE_URL"] = "sqlite:///:memory:"
+    client = app.test_client()
+
+    cleanup()
+
+    db.create_all()
+
+    yield client
+
+def cleanup():
+    db.drop_all()
+
+def test_index_not_logged_in(client):
+    response = client.get('/')
+    #print(response.data)
+    assert b'Das ist die Startseite' in response.data
